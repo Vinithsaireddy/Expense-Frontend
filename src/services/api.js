96e8e3a571e47a8ef28bdda17e49da59486dcc5a
G Vinith Sai Reddy <148205482+Vinithsaireddy@users.noreply.github.com>
2025-06-01 22:13:37 +0530

    Update api.js's backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://expense-backend-4zjs.onrender.com';
 
 const handleResponse = async (response) => {
   const contentType = response.headers.get('content-type');
